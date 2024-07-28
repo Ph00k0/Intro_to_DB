0baa2e514f531927f6525e289bf737c253e9b7fa
root <Ph00k0>
2024-07-28 12:29:38 -0700

    Add task_4.sql to print the full description of the Books table

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Column Type',
+    IS_NULLABLE AS 'Is Nullable',
+    COLUMN_DEFAULT AS 'Default Value',
+    EXTRA AS 'Extra Information'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books'
+ORDER BY 
+    ORDINAL_POSITION;
+
